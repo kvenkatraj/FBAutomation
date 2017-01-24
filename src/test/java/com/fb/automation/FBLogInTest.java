9fac55d2eeb66da968f138f04ce56d48f47bf11d
venkat <kvenkatraj@gmail.com>
2017-01-24 14:36:19 -0500

    project commit

---
+++ src/test/java/com/fb/automation/FBLogInTest.java
@@ -0,0 +1,17 @@
+package com.fb.automation;
+
+import org.testng.annotations.Test;
+
+public class FBLogInTest {
+	
+	
+	@Test
+	public void testLogin(){
+		
+		driver.getC\
+		
+	}
+	
+	
+
+}
---
+++ src/test/java/com/fb/automation/LogInTest.java
@@ -0,0 +1,46 @@
+package com.fb.automation;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.apache.poi.xssf.usermodel.XSSFRow;
+import org.apache.poi.xssf.usermodel.XSSFSheet;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class LogInTest {
+	@Test
+	public void testLogIn(){
+		
+		System.setProperty("webdriver.chrome.driver", "D:/venkat/javatraining/ws/MyFirstProject/lib/chromedriver.exe");
+		
+		WebDriver driver = new ChromeDriver();
+		
+		
+		
+	}
+	
+	@Test
+	public void getTestData() throws Exception{
+		
+		XSSFWorkbook workBook = new XSSFWorkbook("src/test/resources/UserData.xlsx");		
+		XSSFSheet sheet = workBook.getSheetAt(0);		
+				
+		int rows = sheet.getPhysicalNumberOfRows();
+		
+		for( int i=0; i < rows; i++){		
+			XSSFRow row = sheet.getRow(i);
+			String userName = row.getCell(0).getStringCellValue();
+			String password = row.getCell(1).getStringCellValue();			
+			System.out.println(userName);
+			System.out.println(password);
+			
+		}
+		
+		workBook.close();
+		
+	}
+
+}
